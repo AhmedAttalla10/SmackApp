b4f825c198c87de78c8989e9e2c845846cf3c4e9
hima attalla <ahmedatalla2018@gmail.com>
2019-12-31 14:14:40 -0800

    second commit

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by hima attalla on 12/30/19.
+//  Copyright © 2019 ahmed attalla. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+
+    
+
+}
